98958b42639694cf1095632e96bab15fd061221f
BipinKalra <bipinkalra@gmail.com>
2021-05-17 18:22:58 +0530

    added: basic colors and design elements

--- src/views/main.tsx
+++ src/views/main.tsx
@@ -1,3 +1,19 @@
 import React from 'react';
 
-export const MainView: React.FC = () => <h1 className="red"> Hello World !</h1>
+export const MainView: React.FC = () => (
+  <>
+    <div className="ide-container ide-container--dark">
+      <div className="navbar-top">
+        <div className="width-limiter">
+          <div className="row no-gutters align-items-center justify-content-between py-4 px-5">
+            <img
+              src="http://pngimg.com/uploads/amazon/amazon_PNG6.png"
+              alt="Coding Minutes Logo"
+              style={{ height: '50px' }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+);
