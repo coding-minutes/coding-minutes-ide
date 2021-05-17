a1ee19f29fc50e1fd1eec4874a93ca2c283ac6a0
BipinKalra <bipinkalra@gmail.com>
2021-05-17 21:42:38 +0530

    added: basic UI

--- src/views/main.tsx
+++ src/views/main.tsx
@@ -4,13 +4,42 @@ export const MainView: React.FC = () => (
   <>
     <div className="ide-container ide-container--dark">
       <div className="navbar-top">
-        <div className="width-limiter">
-          <div className="row no-gutters align-items-center justify-content-between py-4 px-5">
+        <div className="width-limiter h-inherit">
+          <div className="row no-gutters align-items-center justify-content-between py-4 px-5 h-inherit">
             <img
-              src="http://pngimg.com/uploads/amazon/amazon_PNG6.png"
+              src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/random.png"
               alt="Coding Minutes Logo"
               style={{ height: '50px' }}
             />
+            <div className="language-selector">
+              <div className="language-selector-placeholder">Select Language</div>
+              <div className="language-selector__content">
+                <div className="language-selector__content__item py-2">Python</div>
+                <div className="language-selector__content__item py-2">Python</div>
+                <div className="language-selector__content__item py-2">Python</div>
+                <div className="language-selector__content__item py-2">Python</div>
+                <div className="language-selector__content__item py-2">Python</div>
+                <div className="language-selector__content__item py-2">Python</div>
+                <div className="language-selector__content__item py-2">Python</div>
+                <div className="language-selector__content__item py-2">Python</div>
+                <div className="language-selector__content__item py-2">Python</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="main-container row no-gutters">
+        <div className="flex-1 ide-section">
+          {/* The IDE goes here */}
+          {/* <div style={{background: "white", height: "100%", width: "100%"}}></div> */}
+        </div>
+        <div className="io-section io-section--hidden">
+          <div className="io-navigation">
+            <div className="tab active">Input</div>
+            <div className="tab">Console</div>
+          </div>
+          <div className="run-button">
+            <img src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/tick-white.svg" />
           </div>
         </div>
       </div>
