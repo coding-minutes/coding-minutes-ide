d1f4d1da0edfd2b6acb3ded355e2721896c48c4e
BipinKalra <bipinkalra@gmail.com>
2021-05-22 21:56:06 +0530

    fix: reset button original state

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -6,20 +6,20 @@ export const Navbar: React.FC = () => (
   <div className="navbar-top">
     <div className="h-inherit">
       <div className="row no-gutters align-items-center justify-content-between py-2 px-5 h-inherit">
-        <img
-          src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/cm-oneline.svg"
-          alt="Coding Minutes Logo"
-          style={{ height: '20px' }}
-        />
+        <a href="https://codingminutes.com/" target="_blank">
+          <img
+            src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/cm-oneline.svg"
+            alt="Coding Minutes Logo"
+            style={{ height: '20px' }}
+          />
+        </a>
         <div className="row no-gutters align-items-center">
           <LanguagePicker />
           <div className="open-io-button pulse">
             <div>&gt;_</div>
             {/* <div>&lt;/&gt;</div> */}
 
-            <div className="open-io-button__tooltip">
-              Toggle I/O pane
-            </div>
+            <div className="open-io-button__tooltip">Toggle I/O pane</div>
           </div>
         </div>
       </div>
