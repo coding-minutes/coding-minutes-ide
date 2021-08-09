3d398f01f459f92ddcfdd4824c82ab42bcad1629
Tarun Luthra <tarunluthra987@gmail.com>
2021-08-09 15:57:28 +0530

    Banner data refractoring

--- src/components/layout/banner.tsx
+++ src/components/layout/banner.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import { toggleBanner } from '~/store/action/ui';
 import { useDispatch } from 'react-redux';
-import data from '~/data/banner.json';
 import Markdown from 'markdown-to-jsx';
 
-function pickRandomBannerData(data) {
-  const keys = Object.keys(data);
-  console.log(keys);
-  return data[keys[(keys.length * Math.random()) << 0]];
+interface Props {
+  content: string;
+  link: string;
 }
 
-interface Props {}
-
-export const Banner: React.FC<Props> = (props) => {
+export const Banner: React.FC<Props> = ({ link, content }) => {
   const dispatch = useDispatch();
-  const { content, link } = pickRandomBannerData(data);
 
   function closeBanner() {
     dispatch(toggleBanner());
