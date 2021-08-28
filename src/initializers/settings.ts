172eb97e6d763ba7cb23f0caca56dc451532a234
Tarun Luthra <tarunluthra987@gmail.com>
2021-08-28 12:16:25 +0530

    Font size dispatch fix

--- src/initializers/settings.ts
+++ src/initializers/settings.ts
@@ -1,10 +1,13 @@
 import { Dispatch } from 'redux';
 import { getSettings } from '~/services/settings';
-import { setLanguageById } from '~/store/action/editor';
+import { setLanguageById, setFontSize } from '~/store/action/editor';
 
 export const loadSettings = (dispatch: Dispatch) => {
   const settings = getSettings();
   if (settings?.lang_id) {
     dispatch(setLanguageById(settings.lang_id));
+  }
+  if (settings?.fontSize) {
+    dispatch(setFontSize(settings.fontSize));
   }
 };
