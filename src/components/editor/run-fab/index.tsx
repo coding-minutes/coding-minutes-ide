e31becd1235f213f8cf7dd83f2bd8f064841adef
Jatin Katyal <jatin.katyal13@gmail.com>
2021-09-22 20:52:45 +0530

    Hotfix: Ide end run state

--- src/components/editor/run-fab/index.tsx
+++ src/components/editor/run-fab/index.tsx
@@ -43,7 +43,7 @@ export const RunFAB = (props: RunFabProps) => {
       yield sleep(1000);
       const submission: Submission = yield pollSubmission(submissionId);
 
-      if (submission.compile_output || submission.stdout || submission.stderr) {
+      if (submission.time) {
         setOutput(submission.compile_output || submission.stderr || submission.stdout);
         setCode(submission.stderr || submission.compile_output ? 1 : 0);
         return;
