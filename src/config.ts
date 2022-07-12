696b8fca686c7c0273b4b69c880a4ada9d55ebdb
Jatin Katyal <jatin.katyal13@gmail.com>
2022-07-13 00:45:10 +0530

    Fix: environment name can't start with GITHUB

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export default {
   API: {
     HOST: process.env.API_HOST || 'http://localhost:9000',
   },
-  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  GOOGLE_CLIENT_ID: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+  GITHUB_CLIENT_ID: process.env.REACT_APP_GITHUB_CLIENT_ID,
 };
